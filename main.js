0781ea2ebc8cfe3383d29690b6e483240aaa7e9e
Zemerpe <114874015+Zemerpe@users.noreply.github.com>
2026-01-20 19:03:14 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.182.0/build/three.module.min.js';
 import { ARButton } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/webxr/ARButton.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
 
@@ -107,3 +107,4 @@ document.getElementById('startBtn').onclick = () => {
     initAR();
     document.getElementById('startBtn').style.display = 'none';
 };
+
